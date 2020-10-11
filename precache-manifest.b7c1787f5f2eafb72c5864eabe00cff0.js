197ae77a08b0644424739b3a3314c79a19621286
Carolina Vale Rosa <carolinavalerosa@gmail.com>
2020-10-11 11:01:24 +0200

    Initial commit

---
+++ precache-manifest.b7c1787f5f2eafb72c5864eabe00cff0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "741155b81d9d56f48eec",
+    "url": "/css/app.5eb0dab1.css"
+  },
+  {
+    "revision": "de253d93979a7c0553147f2db6714847",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87af29fd0aa0660f1ffa",
+    "url": "/js/about.a95936f7.js"
+  },
+  {
+    "revision": "741155b81d9d56f48eec",
+    "url": "/js/app.aaa5bb53.js"
+  },
+  {
+    "revision": "eb148008b1ea8836300d",
+    "url": "/js/chunk-vendors.0690bffd.js"
+  },
+  {
+    "revision": "6a56d55230f038df0f4aea082e3e5797",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
